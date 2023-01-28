cb7d42709d2cbd58eafd44eaadcc62277f6d9267
Riyad Hossain <riyad.hossain114@gmail.com>
2023-01-28 01:51:01 -0700

    edits

--- src/Components/Quora/Feed.js
+++ src/Components/Quora/Feed.js
@@ -1,26 +1,31 @@
 
 import Project from './Project'
-import data from '../../Data/intro.json'
+import intro from '../../Data/intro.json'
 import musicsurf from '../../Data/musicsurf.json'
 import saladtime from '../../Data/saladtime.json'
 import art from '../../Data/art.json'
 import stonekeep from '../../Data/stonekeep.json'
-
+import oms from '../../Data/oms.json'
+import featurenet from '../../Data/featurenet.json'
 
 import { useState } from 'react';
 
 function Feed() {
     return (
         <div className="feed">
-            <Project data={data} /> 
+            <Project data={intro} />
+            <Project data={oms} /> 
+            <Project data={featurenet} />
             <Project data={musicsurf} />
-            <Project data={saladtime} />
             <Project data={stonekeep} />
+            <Project data={saladtime} />
             <Project data={art} />
         </div>
        
 
     );
 }
+
+
 
 export default Feed;
--- src/Components/Quora/NavBar.js
+++ src/Components/Quora/NavBar.js
@@ -23,7 +23,7 @@ function NavBar() {
                                 <a className="nav-link" href="#">Spotify Theme</a>
                             </div>
                             <div className="col-md-3">
-                            <a className="mr-4 hover-css" data-content="Download My Resume" href="./RH Resume-3.pdf" download="RH_Resume"><img className="icons" src={process.env.PUBLIC_URL + "./icons/download.png"} /></a>
+                            <a className="mr-4 hover-css" data-content="Download Resume" href="./RH Resume-3.pdf" download="RH_Resume"><img className="icons" src={process.env.PUBLIC_URL + "./icons/download.png"} /></a>
                             <a className="mr-4 hover-css" data-content="GitHub" href="https://github.com/riyadhossain1998" title="github icons"><img className="icons" src={process.env.PUBLIC_URL + "./icons/github.png"}/></a>
                             <a className="mr-4 hover-css" data-content="LinkedIn" href="https://linkedin.com/in/riyad-hossain-30773914b" title="linkedin icons"><img className="icons" src={process.env.PUBLIC_URL + "./icons/linkedin.png"}/></a>
 
